2b86cbeb201177b7405b2ed92b4e75f590e65fd5
KulBoy6122 <107022693+KulBoy6122@users.noreply.github.com>
2025-07-07 12:17:16 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,8 +9,8 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/KulBoy6122",
     resume:
-      "https://drive.google.com/file/d/1XFWqM6qBlIFINjz5z60AQ9J_0jGH2Pzm/view?usp=sharing",
-    linkedin: "https://www.linkedin.com/in/sarthak-kulkarni-336910200/",
+      "https://drive.google.com/file/d/17fSfRhwjCLZ5fOYEcuNULYO-F8GZl1OI/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/sarthakkulkarni612/",
     // twitter: "https://twitter.com/RishavChanda",
     insta: "https://www.instagram.com/sarthak_kulkarnii/",
     facebook: "https://www.facebook.com/sarthakkulkarni612/",
